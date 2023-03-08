4179434fb186c29138174decaea15ba30473b74b
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-03-09 00:14:38 +0530

    Initialize backend repo

---
+++ src/app.js
@@ -0,0 +1,15 @@
+// require express
+const express = require('express');
+const port = 8000;
+const router = require('./routes/routes.js');
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World! backend');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
