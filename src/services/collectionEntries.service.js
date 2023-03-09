960f4ed8cc095eb1277c3fb5488469b29148f103
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-03-09 16:17:41 +0530

    feat: update field name in collection-entry when updated in content-type

--- src/services/collectionEntries.service.js
+++ src/services/collectionEntries.service.js
@@ -11,10 +11,10 @@ const getCollectionEntries = () => {
   return db.CollectionEntries.findAll();
 };
 
-const addFieldToCollectionEntry = async (id, fieldName) => {
+const addFieldToCollectionEntry = async (contentTypeId, fieldName) => {
   const gotCollectionEntry = await db.CollectionEntries.findOne({
     where: {
-      id,
+      contentTypeId,
     },
   });
   if (!gotCollectionEntry) {
@@ -29,9 +29,33 @@ const addFieldToCollectionEntry = async (id, fieldName) => {
   return updatedCollectionEntry;
 };
 
+const updateFieldName = async (contentTypeId, oldFieldName, newFieldName) => {
+  const gotCollectionEntry = await db.CollectionEntries.findOne({
+    where: {
+      contentTypeId,
+    },
+  });
+  if (!gotCollectionEntry) {
+    throw new Error('CollectionEntry not found');
+  }
+  if (!gotCollectionEntry.fields.some((field) => field[0] === oldFieldName)) {
+    throw new Error('Field does not exist in collectionEntry');
+  }
+  const updatedCollectionEntry = await gotCollectionEntry.update({
+    fields: gotCollectionEntry.fields.map((field) => {
+      if (field[0] === oldFieldName) {
+        return [newFieldName, field[1]];
+      }
+      return field;
+    }),
+  });
+  return updatedCollectionEntry;
+};
+
 const CollectionEntriesService = {
   createCollectionEntry,
   getCollectionEntries,
   addFieldToCollectionEntry,
+  updateFieldName,
 };
 module.exports = CollectionEntriesService;
