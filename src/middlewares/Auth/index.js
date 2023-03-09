d187ed4d82dba19224065faed13a89bfb8eed159
Sukhacoder02 <sukhmansindhipb05@gmail.com>
2023-03-09 20:17:33 +0530

    feat: add authentication middleware

---
+++ src/middlewares/Auth/index.js
@@ -0,0 +1,38 @@
+const Joi = require('joi');
+const axios = require('axios');
+
+const tokenSchema = Joi.object({
+  token: Joi.string().required(),
+});
+
+const tokenValidator = () => async (req, res, next) => {
+  try {
+    let token = req.headers['authorization'];
+    token = token && token.split(' ')[1];
+
+    const { error } = tokenSchema.validate({ token });
+    // throw error
+    if (error) {
+      throw new Error('No token provided');
+    }
+
+    const response = await axios.get(
+      'http://localhost:1111/api/token/validate',
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    if (response.status !== 200) {
+      throw new HttpError('invalid token', 401);
+    } else {
+      req.user = response.data.email;
+      next();
+    }
+  } catch (error) {
+    return res.status(401).json({ error: error.message });
+  }
+};
+
+module.exports = tokenValidator;
