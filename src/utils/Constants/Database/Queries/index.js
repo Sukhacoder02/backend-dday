fd3d3d4d21315a5a4b37e461cb2da8a9814517a8
Sukhman S <sukhmansindhipb05@gmail.com>
2023-07-13 17:55:53 +0530

    (feat): add GET_COUNT query

--- src/utils/Constants/Database/Queries/index.js
+++ src/utils/Constants/Database/Queries/index.js
@@ -1,5 +1,8 @@
 module.exports = {
   GET_ALL_COLUMNS: (tableName) => {
     return `SELECT column_name,data_type FROM information_schema.columns WHERE table_schema = 'public' AND table_name = '${tableName}';`;
+  },
+  GET_COUNT: (tableName) => {
+    return `SELECT COUNT(*) FROM "${tableName}";`;
   }
 };
