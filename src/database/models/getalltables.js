1ec8db2d91e4a11ce3073d2a3aa6c9d5630ffced
Sukhman S <sukhmansindhipb05@gmail.com>
2023-07-13 17:55:06 +0530

    (feat): add row_count along with table_name in GET_ALL_TABLES

--- src/database/models/getalltables.js
+++ src/database/models/getalltables.js
@@ -1,19 +1,7 @@
-// const { DataTypes } = require('sequelize');
-// const { Sequelize } = require('./index');
-
-// const GetAllTables = Sequelize.define('GET_ALL_TABLES', {
-//   table_name: {
-//     type: DataTypes.STRING,
-//   }
-// }, {
-//   tableName: 'GET_ALL_TABLES',
-//   timestamps: false
-// });
-
-// module.exports = GetAllTables;
 'use strict';
 const { Model } = require('sequelize');
 const Sequelize = require('sequelize');
+const Queries = require('../../utils/Constants/Database/Queries');
 module.exports = (sequelize, DataTypes) => {
   class GET_ALL_TABLES extends Model {
     /**
@@ -43,8 +31,8 @@ module.exports = (sequelize, DataTypes) => {
       freezeTableName: true
     }
   );
-  GET_ALL_TABLES.FindAll = () => {
-    return GET_ALL_TABLES.findAll({
+  GET_ALL_TABLES.FindAll = async () => {
+    const allTables = await GET_ALL_TABLES.findAll({
       attributes: {
         exclude: ['id']
       },
@@ -54,6 +42,17 @@ module.exports = (sequelize, DataTypes) => {
         }
       }
     });
+    const promises = [];
+    allTables.forEach((tableObject) => {
+      const tableName = tableObject.table_name;
+      promises.push(sequelize.query(Queries.GET_COUNT(tableName)));
+    });
+    const counts = await Promise.all(promises);
+    const result = allTables.map((tableObject, index) => {
+      return { table_name: tableObject.table_name, row_count: counts[index][0][0].count };
+    });
+    return result;
+
   };
   return GET_ALL_TABLES;
 };
