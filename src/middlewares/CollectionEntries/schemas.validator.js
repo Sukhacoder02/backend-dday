2d9a8cb709f756ffea84ea157b18db56658d7338
Sukhman S <sukhmansindhipb05@gmail.com>
2023-07-15 17:46:53 +0530

    (feat): add delete entry schema

--- src/middlewares/CollectionEntries/schemas.validator.js
+++ src/middlewares/CollectionEntries/schemas.validator.js
@@ -5,7 +5,10 @@ const Queries = require('../../utils/Constants/Database/Queries');
 const nameParamSchema = Joi.object({
   name: Joi.string()
 });
-
+const deleteCollectionEntrySchema = Joi.object({
+  name: Joi.string().required(),
+  id: Joi.number().required(),
+}).required();
 const fieldDetailsBodySchema = Joi.object({
   fieldDetails: Joi.object().required(),
 });
@@ -15,21 +18,22 @@ const addNewCollectionEntrySchema = Joi.object({
   fieldDetails: Joi.object().required(),
 });
 
-const getSchemaForCollectionEntry = async (tableName) => {
+const getSchemaForCollectionEntry = async (tableName, required = true) => {
   let result = await db.sequelize.query(Queries.GET_ALL_COLUMNS(tableName));
   result = result[0];
   const schemaObj = {};
   result.forEach((column) => {
     if (column.column_name === 'id') return;
-    schemaObj[column.column_name] = Joi.string().required();
+    schemaObj[column.column_name] = required ? Joi.string().required() : Joi.string();
   });
-  return Joi.object(schemaObj);
+  return required ? Joi.object(schemaObj).required() : Joi.object(schemaObj);
 };
 const Schemas = {
   nameParamSchema,
   fieldDetailsBodySchema,
   addNewCollectionEntrySchema,
-  getSchemaForCollectionEntry
+  getSchemaForCollectionEntry,
+  deleteCollectionEntrySchema
 };
 
 module.exports = Schemas;
